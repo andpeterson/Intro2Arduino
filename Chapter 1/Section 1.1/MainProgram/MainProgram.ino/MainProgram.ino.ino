8b954ca8fb1a8c66fed89dae306f617ef2ac4c80
Andrew Peterson <andrew@andpeterson.com>
2018-03-08 12:03:41 -0800

    Added images for chapter 1.1

---
+++ Chapter 1/Section 1.1/MainProgram/MainProgram.ino/MainProgram.ino.ino
@@ -0,0 +1,14 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(13, OUTPUT); //tells the Arduino that pin 13 will be used as an OUTPUT
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(13, 1); //tells the Arduino to output power out of pin 13
+  digitalWrite(13, 0); //tells the Arduino to stop outputting power out of pin 12
+  delay(1000); //tells the Arduino to wait for 1 second 
+  digitalWrite(13, 0); //tells the Arduino to stop outputting power out of pin 13
+  digitalWrite(12, 1); //tells the Arduino to output power out of pin 13
+  delay(1000); // tells the Arduino to wait for 1 second
+}
